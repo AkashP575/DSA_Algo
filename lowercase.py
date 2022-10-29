345c27938e6e0161a19d5177ea5254b42a6e0873
rnk8383 <116947968+rnk8383@users.noreply.github.com>
2022-10-29 15:16:09 +0530

    Create lowercase.py

---
+++ lowercase.py
@@ -0,0 +1,11 @@
+text = input()
+
+for i in range(len(text)):
+    if text[i]>='a' and text[i]<='z':
+        ch = text[i]
+        ch = ord(ch)
+        ch = ch-32
+        ch = chr(ch)
+        text = text[:i] + ch + text[i+1:]
+
+print(text)
